5a2e0c6422217d3b2680e1eedb104141aab0231e
YasufumiK <1.618ogon@gmail.com>
2024-07-07 18:47:11 +0900

    完全一致部分の修正

--- pswdmng.sh
+++ pswdmng.sh
@@ -1,32 +1,31 @@
 echo パスワードマネージャーへようこそ！
-Choice=1
+Choice=1 #Choiceの初期値．Exit以外ならば何でもよい．
 while [ $Choice != "Exit" ]
 do
     echo "次の選択肢から入力してください(Add Password / Get Password / Exit)："
     read Choice
     if [ $Choice = "Add Password" ] ; then
         echo サービス名を入力してください：
-        read SName
+        read Svs
         echo ユーザー名を入力してください：
-        read UName
+        read Usr
         echo パスワードを入力してください：
-        read Pswd
-        echo $SName:$UName:$Pswd >> secret.txt
+        read Pwd
+        echo $Svs:$Usr:$Pwd >> secret.txt
         echo パスワードの追加は成功しました．
     elif [ $Choice = "Get Password" ] ; then
         echo サービス名を入力してください．
-        read SName
-        CheckSName=$(grep $SName secret.txt | cut -d : -f 1)1
-        if [ "$SName"1 = $CheckSName ] ; then
-            echo サービス名：$SName
-            echo ユーザー名：$(grep $SName secret.txt | cut -d : -f 2)
-            echo パスワード：$(grep $SName secret.txt | cut -d : -f 3)
+        read Svs
+        if [ $Svs = $(grep "^$Svs:" secret.txt | cut -d : -f 1) ] ; then #完全一致しているかのチェック
+            echo サービス名：$Svs
+            echo ユーザー名：$(grep "^$Svs:" secret.txt | cut -d : -f 2)
+            echo パスワード：$(grep "^$Svs:" secret.txt | cut -d : -f 3)
         else
-            echo そのサービス名は登録されていません
+            echo そのサービス名は登録されていません．
         fi
     elif [ $Choice = "Exit" ] ; then
         echo Thank you!
     else
-        echo 入力が間違っています．
+        echo 入力が間違っています．Add Password / Get Password いずれかを入力してください．
     fi
 done
